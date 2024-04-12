173f463f98c63ed9bae266b45c700e332b2e5073
Jan Schwarz <jan-paul@schw-a-rz.de>
2024-04-12 11:41:46 +0200

    add form with minimal styling and functionality on index.js

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,78 @@
-import EmotionList from "@/components/EmotionList";
+import styled from "styled-components";
+import { useState } from "react";
+import useLocalStorageState from "use-local-storage-state";
+import { uid } from "uid";
+
+const StyledForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  width: 80vw;
+  margin: 2rem auto;
+  justify-content: center;
+  align-items: center;
+`;
+
+const StyledLabel = styled.label`
+  padding: 2rem;
+  text-align: center;
+`;
+
+const StyledInput = styled.input`
+  width: inherit;
+`;
+
+const StyledWrapper = styled.div`
+  display: flex;
+  width: inherit;
+  justify-content: space-between;
+`;
+
+const StyledSpan = styled.span`
+  font-size: 0.8rem;
+`;
+
+const StyledButton = styled.button`
+  width: 3rem;
+`;
 
 export default function HomePage() {
+  const [tensionEntry, setTensionEntry] = useLocalStorageState("tensionEntry", {
+    defaultValue: [],
+  });
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData);
+    const date = new Date();
+
+    setTensionEntry(
+      tensionEntry.length === 0
+        ? [{ ...data, id: uid(), date: date.toUTCString() }]
+        : [...tensionEntry, { ...data, id: uid(), date: date.toUTCString() }]
+    );
+  }
+
   return (
     <>
-      <EmotionList />
+      <StyledForm onSubmit={handleSubmit}>
+        <StyledLabel htmlFor="tension-level">
+          On a scale from 0 to 100, how tense do you feel right in this moment?
+        </StyledLabel>
+        <StyledInput
+          id="tension-level"
+          name="tensionLevel"
+          type="range"
+          defaultValue={0}
+          max={100}
+        ></StyledInput>
+        <StyledWrapper>
+          <StyledSpan>0</StyledSpan>
+          <StyledSpan>100</StyledSpan>
+        </StyledWrapper>
+        <StyledButton>Save</StyledButton>
+      </StyledForm>
     </>
   );
 }
