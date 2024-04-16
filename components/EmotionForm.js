5768f9e7e8a3f5a1f5cca7275da2fa3a39842629
Jana May <jana.ricarda.may@gmail.com>
2024-04-16 13:47:26 +0200

    add minimal styleing

--- components/EmotionForm.js
+++ components/EmotionForm.js
@@ -15,6 +15,15 @@ const StyledForm = styled.form`
   justify-content: center;
   gap: 1rem;
 `;
+const StyledWrapper = styled.div`
+  display: flex;
+  width: inherit;
+  justify-content: space-between;
+`;
+
+const StyledSpan = styled.span`
+  font-size: 0.8rem;
+`;
 
 const StyledLink = styled(Link)`
   text-decoration: none;
@@ -26,6 +35,26 @@ const StyledLink = styled(Link)`
     background-color: ${({ $color }) => $color};
   }
 `;
+const StyledSelect = styled.select`
+  border-radius: 6px;
+  padding: 0.3rem 0;
+`;
+
+const StyledTextarea = styled.textarea`
+  border: 1px solid black;
+  border-radius: 6px;
+`;
+
+const StyledSubmitButton = styled.button`
+  background-color: white;
+  border: 1px solid black;
+  border-radius: 6px;
+  padding: 0.5rem;
+  &:hover {
+    background-color: darkslateblue;
+    color: ${({ $color }) => $color};
+  }
+`;
 
 export default function EmotionForm({ name, color }) {
   return (
@@ -34,9 +63,8 @@ export default function EmotionForm({ name, color }) {
       <StyledForm $color={color}>
         <label htmlFor="date">Date and time:</label>
         <input type="date" id="date" name="date"></input>
-
         <label htmlFor="subemotion">Select Subemotion: </label>
-        <select id="subemotion" name="subemotion" required>
+        <StyledSelect id="subemotion" name="subemotion" required>
           <option value="joy">Joy</option>
           <option value="surprise">Surprise</option>
           <option value="fear">Fear</option>
@@ -44,21 +72,41 @@ export default function EmotionForm({ name, color }) {
           <option value="contempt">Contempt</option>
           <option value="disgust">Disgust</option>
           <option value="anger">Anger</option>
-        </select>
-
+        </StyledSelect>
         <label htmlFor="intensity"> Emotion Intensity: </label>
-        <input type="range" id="intensity" name="intensity" required></input>
-
+        <input
+          type="range"
+          id="intensity"
+          name="intensity"
+          defaultValue={0}
+          max={100}
+          required
+        ></input>
+        <StyledWrapper>
+          <StyledSpan>0</StyledSpan>
+          <StyledSpan>100</StyledSpan>
+        </StyledWrapper>
         <label htmlFor="category"> Association Category: </label>
-        <input type="range" id="category" name="category" required></input>
-
-        <label htmlFor="trigger">Trigger: </label>
-        <textarea id="trigger" name="trigger"></textarea>
-
+        <input
+          type="range"
+          id="category"
+          name="category"
+          defaultValue={0}
+          max={100}
+          required
+        ></input>
+        <StyledWrapper>
+          <StyledSpan>0</StyledSpan>
+          <StyledSpan>100</StyledSpan>
+        </StyledWrapper>
+        <label htmlFor="trigger"></label>
+        Trigger:
+        <StyledTextarea id="trigger" name="trigger"></StyledTextarea>
         <label htmlFor="notes">Notes: </label>
-        <textarea id="notes" name="notes"></textarea>
-
-        <button type="submit">Submit</button>
+        <StyledTextarea id="notes" name="notes"></StyledTextarea>
+        <StyledSubmitButton type="submit" $color={color}>
+          Submit
+        </StyledSubmitButton>
       </StyledForm>
       <StyledLink href="/" $color={color}>
         ← Back to Tension Entry
