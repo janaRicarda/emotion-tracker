abaeb24b328969a89b3eae7ca760b56cc66503d7
Jana May <jana.ricarda.may@gmail.com>
2024-05-13 12:13:54 +0200

    add warmTheme, coldTheme, neutralTheme and highContrastTheme to Theme.js

--- components/Theme.js
+++ components/Theme.js
@@ -23,3 +23,55 @@ export const darkTheme = {
   disgust: "hsl(310, 80%, 70%)",
   anger: "hsl(10, 80%, 70%)",
 };
+
+export const warmTheme = {
+  background: "#f7e9cb",
+  text: "#24164a",
+  buttonBackground: "lightskyblue",
+  joy: "#f2ed5c",
+  surprise: "#f7c059",
+  fear: "#f7a559",
+  sadness: "#f78159",
+  contempt: "#f75959",
+  disgust: "#f75986",
+  anger: "#f75986",
+};
+
+export const coldTheme = {
+  background: "#cbf0f7",
+  text: "#09343d",
+  buttonBackground: "lightskyblue",
+  joy: "#b059f7",
+  surprise: "#7159f7",
+  fear: "#5971f7",
+  sadness: "#59a5f7",
+  contempt: "#59ddf7",
+  disgust: "#47d6cd",
+  anger: "#47d6a4",
+};
+
+export const neutralTheme = {
+  background: "white",
+  text: "black",
+  buttonBackground: "lightgrey",
+  joy: "white",
+  surprise: "white",
+  fear: "white",
+  sadness: "white",
+  contempt: "white",
+  disgust: "white",
+  anger: "white",
+};
+
+export const highContrastTheme = {
+  background: "black",
+  text: "white",
+  buttonBackground: "yellow",
+  joy: "yellow",
+  surprise: "yellow",
+  fear: "yellow",
+  sadness: "yellow",
+  contempt: "yellow",
+  disgust: "yellow",
+  anger: "yellow",
+};
