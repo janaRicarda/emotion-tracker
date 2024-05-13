948de52ca48e35ea3d965913fa9437dae8000fdb
Jana May <jana.ricarda.may@gmail.com>
2024-05-13 10:41:28 +0200

    refectoring manualData

--- components/Manual.js
+++ components/Manual.js
@@ -1,6 +1,5 @@
 import styled from "styled-components";
-import { StyledTitle } from "@/SharedStyledComponents";
-import Heart from "../public/heart-filled.svg";
+import { manualData } from "@/lib/db";
 
 const StyledSection = styled.section`
   width: 100%;
@@ -91,22 +90,6 @@ const StyledOl = styled.ol`
   }
 `; */
 
-/* const StyledHeartArticle = styled.article`
-  //position: relative;
-  position: absolute;
-  left: calc(50% - 2rem);
-  bottom: -19rem;
-`; */
-
-const StyledHeart = styled(Heart)`
-  width: 4rem;
-  height: 4rem;
-  fill: var(--button-background);
-  position: absolute;
-  left: calc(50% - 2rem);
-  bottom: -20rem;
-`;
-
 const StyledListItem = styled.li`
   counter-increment: item;
   b//ackground-color: var(--section-background);
@@ -191,103 +174,18 @@ export default function Manual() {
           effectively:
         </StyledText>
 
-        {/* <details>
-        <summary>Purpose of the app</summary>
-      </details> */}
-        <StyledDetails>
-          <StyledSummary>How can I make a new entry?</StyledSummary>
-          <StyledOl>
-            <StyledListItem>
-              Upon launching the app, you'll be prompted to make a new entry.
-            </StyledListItem>
-            <StyledListItem>
-              Start by assessing your level of tension on a scale from 0 to 100.
-            </StyledListItem>
-            <StyledListItem>
-              Choose whether you'd like to conclude your entry there or delve
-              deeper into your emotions.
-            </StyledListItem>
-            <StyledListItem>
-              If you opt to continue, you'll be asked to select the basic
-              emotion closest to what you're feeling from the following options:
-              joy, surprise, fear, sadness, contempt, disgust or anger.
-            </StyledListItem>
-            <StyledListItem>
-              Once you've made your selection, you'll proceed to a questionnaire
-              where you can assign a sub-emotion to your feeling.
-            </StyledListItem>
-            <StyledListItem>
-              Classify whether your feeling is perceived as negative, neutral,
-              or positive.
-            </StyledListItem>
-            <StyledListItem>
-              Optionally, you can provide details about triggers or additional
-              notes.
-            </StyledListItem>
-            <StyledListItem>
-              After completing the questionnaire, save your entry.
-            </StyledListItem>
-          </StyledOl>
-        </StyledDetails>
-        <StyledDetails>
-          <StyledSummary>What if my entry was wrong?</StyledSummary>
-          <StyledText>
-            If you feel that your entry was incorrect or want to make changes,
-            you can edit or delete it.
-          </StyledText>
-          <StyledOl>
-            <StyledListItem>
-              You can navigate through the app by clicking on the burger menu in
-              the top right corner.
-            </StyledListItem>
-            <StyledListItem>
-              Click on „emotion records“ to go to your Emotion Records List.
-            </StyledListItem>
-            <StyledListItem>
-              Simply locate the entry in your Emotion Records List, tap on the
-              pencil Icon to edit, or tap the trashcan Icon to delete it.
-            </StyledListItem>
-            <StyledListItem>
-              For safety-reasons you will be asked to confirm your decision
-              before changing your data.
-            </StyledListItem>
-            <StyledListItem>
-              Please note that there is no option to restore your entries once
-              they’re deleted.
-            </StyledListItem>
-          </StyledOl>
-        </StyledDetails>
-        <StyledDetails>
-          <StyledSummary>How to answer the question?</StyledSummary>
-          <StyledOl>
-            <StyledListItem>
-              When answering the question about which basic emotion your feeling
-              resembles the most, choose the one that closely aligns with your
-              current emotional state.
-            </StyledListItem>
-            <StyledListItem>
-              Be honest and select the emotion that best describes your feelings
-              at the moment.
-            </StyledListItem>
-            <StyledListItem>
-              If you're unsure, take a moment to reflect on your emotions before
-              making your choice.
-            </StyledListItem>
-            <StyledListItem>
-              If needed you can get more information about the 7 basic emotions.
-              Therefor simply go to the 7 basic emotions via the burger menu.
-            </StyledListItem>
-            <StyledListItem>
-              The 7 basic emotions show up as a list and you can click on any of
-              the emotions to get more details.
-            </StyledListItem>
-            <StyledListItem>
-              Through the next and previous buttons on the side, you can click
-              yourself through all of the emotions without returning to the
-              emotions list all the time.
-            </StyledListItem>
-          </StyledOl>
-        </StyledDetails>
+        {manualData.map(({ question, text, answers }) => (
+          <StyledDetails key={question}>
+            <StyledSummary>{question}</StyledSummary>
+            <StyledText>{text}</StyledText>
+            <StyledOl>
+              {answers.map((answer) => (
+                <StyledListItem key={answer}>{answer}</StyledListItem>
+              ))}
+            </StyledOl>
+          </StyledDetails>
+        ))}
+
         <StyledText>
           Remember, the What a feeling app is here to assist you in
           understanding and managing your emotions. Feel free to explore its
@@ -295,7 +193,6 @@ export default function Manual() {
           and gain insights into the basic emotions.
         </StyledText>
         <StyledText>Happy feeling!</StyledText>
-        {/* <StyledHeart /> */}
       </StyledSection>
     </>
   );
