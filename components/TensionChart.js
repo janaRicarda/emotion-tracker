b59f790190f275e2c3f017bf965593e478e5d08d
Christof Röhl <userpattern@gmail.com>
2024-05-17 17:24:16 +0200

    add little theme tool for chart

--- components/TensionChart.js
+++ components/TensionChart.js
@@ -1,17 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import Plot from "react-plotly.js";
 
-//testing ways to work with the color themes
 import { darkTheme, lightTheme } from "./Theme";
 
-const lightThemeBgColor = "#f1eaea";
-
-function convertThemeColors(theme) {
-  const themeColors = "test";
-}
 // console.log(theme);
 export default function TensionChart({ xValues, yValues, title, theme }) {
+  const [themeForChart, chooseThemeForChart] = useState(lightTheme);
+  function themeSwitch(theme) {
+    theme === "light"
+      ? chooseThemeForChart(lightTheme)
+      : chooseThemeForChart(darkTheme);
+  }
+  useEffect(() => {
+    themeSwitch(theme);
+  }, [theme]);
+
   return (
     <>
       <Plot
@@ -21,18 +25,21 @@ export default function TensionChart({ xValues, yValues, title, theme }) {
             y: yValues,
             type: "scatter",
             mode: "lines+markers",
-            marker: { color: "lightskyblue" },
+            marker: { color: themeForChart.buttonBackground },
           },
         ]}
         layout={{
-          font: { color: "#030352" },
-          paper_bgcolor: lightTheme.background,
+          font: { color: themeForChart.text },
+          paper_bgcolor: themeForChart.background,
           border_radius: 6,
-          plot_bgcolor: "#f1eaea",
+          plot_bgcolor: themeForChart.background,
           width: 320,
           height: 300,
           title: {
-            font: { family: "system-ui", weight: "bold", color: "#030352" },
+            font: {
+              family: "system-ui",
+              weight: "bold",
+            },
           },
           autosize: true,
           margin: { b: 50, l: 30, r: 30, t: 50 },
