bf7404ac29fc1fc80c82f219f9bd277c69ae18a6
Christof Röhl <userpattern@gmail.com>
2024-05-17 13:07:39 +0200

    style chart

--- components/TensionChart.js
+++ components/TensionChart.js
@@ -2,6 +2,15 @@ import React from "react";
 
 import Plot from "react-plotly.js";
 
+//testing ways to work with the color themes
+import { lightTheme } from "./Theme";
+
+const lightThemeBgColor = "#f1eaea";
+
+function convertThemeColors(theme) {
+  const themeColors = "test";
+}
+
 export default function TensionChart({ xValues, yValues, title }) {
   return (
     <>
@@ -12,15 +21,21 @@ export default function TensionChart({ xValues, yValues, title }) {
             y: yValues,
             type: "scatter",
             mode: "lines+markers",
-            marker: { color: "var(--main-dark)" },
-            background: { color: "transparent" },
+            marker: { color: "lightskyblue" },
           },
         ]}
         layout={{
+          font: { color: "#030352" },
+          paper_bgcolor: lightTheme.background,
+          border_radius: 6,
+          plot_bgcolor: "#f1eaea",
           width: 320,
           height: 300,
+          title: {
+            font: { family: "system-ui", weight: "bold", color: "#030352" },
+          },
           autosize: true,
-          margin: { b: 40, l: 40, r: 40, t: 40 },
+          margin: { b: 50, l: 30, r: 30, t: 50 },
           title: title,
         }}
       />
