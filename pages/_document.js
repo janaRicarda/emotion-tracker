21851cf7515a69f822b8f4f4164987768c01f916
Jana May <jana.ricarda.may@gmail.com>
2024-05-23 11:12:38 +0200

    add overscroll behavior to html

--- pages/_document.js
+++ pages/_document.js
@@ -25,7 +25,7 @@ export default class MyDocument extends Document {
 
   render() {
     return (
-      <Html lang="en">
+      <Html style={(overscrollBehavior = "none")} lang="en">
         <Head />
         <body>
           <Main />
--- pages/emotions/index.js
+++ pages/emotions/index.js
@@ -3,19 +3,14 @@ import {
   StyledFlexColumnWrapper,
   StyledFixedTitle,
 } from "@/SharedStyledComponents";
-import styled from "styled-components";
-
-const StyledListWrapper = styled(StyledFlexColumnWrapper)`
-  overscroll-bahavior: none;
-`;
 
 export default function EmotionListPage() {
   return (
     <>
       <StyledFixedTitle>The 7 basic emotions</StyledFixedTitle>
-      <StyledListWrapper>
+      <StyledFlexColumnWrapper>
         <EmotionList />
-      </StyledListWrapper>
+      </StyledFlexColumnWrapper>
     </>
   );
 }
