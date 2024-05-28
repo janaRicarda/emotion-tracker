0da6e69164ae299527f9feea0ae55924e31972d9
Jan Schwarz <jan-paul@schw-a-rz.de>
2024-05-28 14:08:42 +0200

    endPoint added on mondoDB, env.local, connect.js and EmotionEntry Schema added

---
+++ db/models/emotionEntry.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const EmotionEntrySchema = new Schema({
+  tensionLevel: { type: String },
+  timeAndDate: { type: String },
+  isoDate: { type: String },
+  emotion: { type: String },
+  subemotion: { type: String },
+  intensity: { type: String },
+  trigger: { type: String },
+  notes: { type: String },
+});
+
+const EmotionEntry =
+  mongoose.models.EmotionEntry ||
+  mongoose.model("EmotionEntry", EmotionEntrySchema);
+
+export default EmotionEntry;
