bce19e4890f52e03fd23a80ab7e909a4bd5e44db
Sonja Lerchl <sosobee@web.de>
2024-06-03 12:07:13 +0200

    add stuff

--- pages/app-manual.js
+++ pages/app-manual.js
@@ -1,5 +1,14 @@
 import Manual from "@/components/Manual";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 
 export default function ManualPage() {
   return <Manual />;
+}
+export async function getStaticProps({ locale }) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, "manual")),
+      // Will be passed to the page component as props
+    },
+  };
 }
