1bb83d28a423fdeb9b18417bed1192e66906e45f
Christof Röhl <userpattern@gmail.com>
2024-06-10 12:15:17 +0200

    cleanup index.js

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,7 @@
 import styled from "styled-components";
 import { useEffect, useState } from "react";
 import { uid } from "uid";
-import {
-  StyledWrapper,
-  StyledButton,
-  StyledStandardLink,
-  StyledInput,
-  StyledForm,
-  StyledFlexColumnWrapper,
-  StyledTitle,
-} from "@/SharedStyledComponents";
+import { StyledStandardLink, StyledTitle } from "@/SharedStyledComponents";
 
 import { breakpoints } from "@/utils/breakpoints";
 import Head from "next/head";
@@ -19,26 +11,6 @@ import {
   getAveragePerDay,
   getTimeSinceLastEntry,
 } from "@/utils/dataAndChartUtils";
-
-const StyledTensionForm = styled(StyledForm)`
-  margin: 1rem;
-  padding: 1rem;
-  align-items: center;
-  width: 80vw;
-  background: var(--section-background);
-  border-radius: 6px;
-  @media ${breakpoints.mobileLandscape} {
-    height: 60vh;
-    padding: 1rem;
-    margin-top: 0;
-  }
-  @media ${breakpoints.tablet} {
-    width: 60vw;
-  }
-  @media ${breakpoints.laptop} {
-    width: 40vw;
-  }
-`;
 
 const ToggleSwitchWrapper = styled.div`
   display: flex;
@@ -81,55 +53,6 @@ const StyledInfoBox = styled.div`
   & > :nth-child(2) {
     margin-top: 1rem;
   }
-`;
-
-const StyledTensionLabel = styled.label`
-  padding: 1rem 1rem 2rem;
-  text-align: center;
-`;
-
-const StyledSpan = styled.span`
-  padding: 0.6rem 0 0;
-  font-size: 1.2rem;
-`;
-
-const StyledTensionDisplay = styled.p`
-  font-size: 1.2rem;
-  margin: 0 0 1rem;
-`;
-
-const StyledMessage = styled.p`
-  align-self: center;
-  text-align: center;
-  font-weight: 600;
-  margin: 1rem auto;
-`;
-
-const StyledButtonWrapper = styled(StyledWrapper)`
-  justify-content: center;
-`;
-
-const SaveButton = styled(StyledButton)`
-  border: none;
-`;
-
-const StyledBackButton = styled.input`
-  width: 10rem;
-  text-decoration: none;
-  color: var(--contrast-text);
-  margin: 0.5rem;
-  padding: 0.5rem;
-  border-radius: 6px;
-  border-style: none;
-  text-align: center;
-  background-color: var(--button-background);
-`;
-const StyledAddDetailsLink = styled(StyledStandardLink)`
-  color: var(--contrast-text);
-  width: 10rem;
-  margin: 0.5rem;
-  padding: 0.5rem;
-  background-color: var(--button-background);
 `;
 
 const DashboardSection = styled.section`
@@ -200,8 +123,6 @@ export default function HomePage({
   useExampleData,
 }) {
   const [isFormSubmitted, setIsFormSubmitted] = useState(false);
-  const [id, setId] = useState();
-  const [tension, setTension] = useState(0);
   const [showInfoBox, setShowInfoBox] = useState(false);
 
   //for dashboard
@@ -233,6 +154,33 @@ export default function HomePage({
       <Head>
         <title>Home</title>
       </Head>
+
+      <ToggleSwitchWrapper>
+        <StyledInfoIcon
+          onClick={() => {
+            setShowInfoBox(!showInfoBox);
+          }}
+        >
+          &#8505;
+        </StyledInfoIcon>
+        <StyledInfoBox
+          onClick={() => {
+            setShowInfoBox(false);
+          }}
+          $show={showInfoBox}
+        >
+          <span>
+            OFF: Displayed Data is real and comes from your own database.
+          </span>
+          <span> ON: Data is generated locally and fictional.</span>
+        </StyledInfoBox>
+        <ToggleSwitch
+          handleSwitch={toggleExampleData}
+          isChecked={useExampleData}
+          text={"Use Example data"}
+        />
+      </ToggleSwitchWrapper>
+
       <StyledTitle>Dashboard</StyledTitle>
       <DashboardSection>
         <GridElement>
@@ -265,84 +213,6 @@ export default function HomePage({
         {/* <GridElement>5</GridElement>
         <GridElement>6</GridElement> */}
       </DashboardSection>
-      <StyledFlexColumnWrapper>
-        <ToggleSwitchWrapper>
-          <StyledInfoIcon
-            onClick={() => {
-              setShowInfoBox(!showInfoBox);
-            }}
-          >
-            &#8505;
-          </StyledInfoIcon>
-          <StyledInfoBox
-            onClick={() => {
-              setShowInfoBox(false);
-            }}
-            $show={showInfoBox}
-          >
-            <span>
-              OFF: Displayed Data is real and comes from your own database.
-            </span>
-            <span> ON: Data is generated locally and fictional.</span>
-          </StyledInfoBox>
-          <ToggleSwitch
-            handleSwitch={toggleExampleData}
-            isChecked={useExampleData}
-            text={"Use Example data"}
-          />
-        </ToggleSwitchWrapper>
-        <StyledTensionForm onSubmit={handleSubmit}>
-          <StyledTensionLabel htmlFor="tension-level">
-            On a scale from 0 to 100, how tense do you feel in this moment?
-          </StyledTensionLabel>
-          <StyledInput
-            aria-label="Adjust tension level between 0 and 100"
-            id="tension-level"
-            name="tensionLevel"
-            type="range"
-            value={tension}
-            $value={tension}
-            max={100}
-            onChange={(event) => setTension(event.target.value)}
-          />
-          <StyledWrapper>
-            <StyledSpan>0</StyledSpan>
-            <StyledSpan>100</StyledSpan>
-          </StyledWrapper>
-
-          {!isFormSubmitted && (
-            <>
-              <StyledTensionDisplay>{tension}</StyledTensionDisplay>
-              <SaveButton type="submit">Save</SaveButton>
-            </>
-          )}
-
-          {isFormSubmitted && (
-            <>
-              <StyledMessage>Your entry was successfully saved!</StyledMessage>
-              <StyledButtonWrapper>
-                <StyledBackButton
-                  type="reset"
-                  value={"Done"}
-                  onClick={() => {
-                    setIsFormSubmitted(!isFormSubmitted);
-                    setTension("0");
-                  }}
-                />
-                <StyledAddDetailsLink
-                  href={{
-                    pathname: "/create",
-                    query: { id: useExampleData ? id : newestDbEntryID },
-                  }}
-                  forwardedAs={`/create`}
-                >
-                  Add more details
-                </StyledAddDetailsLink>
-              </StyledButtonWrapper>
-            </>
-          )}
-        </StyledTensionForm>
-      </StyledFlexColumnWrapper>
     </>
   );
 }
