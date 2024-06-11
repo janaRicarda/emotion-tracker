2012b9b6b94004f12bc6a9c7dca43e8802b1f61d
Sonja Lerchl <sosobee@web.de>
2024-06-11 12:39:33 +0200

    pushing error for fixing

--- pages/edit/[id]/index.js
+++ pages/edit/[id]/index.js
@@ -1,8 +1,16 @@
 import EmotionForm from "@/components/EmotionForm";
 import { useRouter } from "next/router";
-import { useEffect, useTransition } from "react";
+import { useEffect } from "react";
 import Head from "next/head";
 import { useTranslation } from "next-i18next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+
+async function fetchEmotionEntries() {
+  return [
+    { id: "1", _id: "1", emotion: "joy" },
+    { id: "2", _id: "2", emotion: "sadness" },
+  ];
+}
 
 export default function EditPage({
   theme,
@@ -25,6 +33,10 @@ export default function EditPage({
     ? emotionEntries.find((entry) => entry.id === id)
     : emotionEntries.find((entry) => entry._id === id);
 
+  // console.log("ID from router query:", id);
+  // console.log("Emotion Entries:", emotionEntries);
+  // console.log("Corresponding Entry:", correspondingEntry);
+
   if (!correspondingEntry) return <h2>Page not found!</h2>;
 
   return (
@@ -41,4 +53,30 @@ export default function EditPage({
       />
     </>
   );
+}
+
+export async function getStaticProps({ locale, params }) {
+  const emotionEntries = await fetchEmotionEntries();
+  return {
+    props: {
+      emotionEntries,
+      ...(await serverSideTranslations(locale, [
+        "emotions",
+        "common",
+        "navigation",
+      ])),
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  const emotionEntries = await fetchEmotionEntries();
+  const paths = emotionEntries.map((entry) => ({
+    params: { id: entry.id },
+  }));
+
+  return {
+    paths,
+    fallback: true, // Set to true if you want to enable ISR (Incremental Static Regeneration)
+  };
 }
