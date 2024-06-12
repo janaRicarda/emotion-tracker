b759d6255b2644f34b7f49ce42af7aea3f877ad4
Sonja Lerchl <sosobee@web.de>
2024-06-12 10:16:14 +0200

    commit to check error message translation

--- components/ErrorMessage.js
+++ components/ErrorMessage.js
@@ -5,6 +5,7 @@ import {
 } from "@/SharedStyledComponents";
 import styled from "styled-components";
 import { useRouter } from "next/router";
+import { useTranslation } from "next-i18next";
 
 const StyledMessageWrapper = styled(StyledFlexColumnWrapper)`
   height: fit-content;
@@ -29,11 +30,13 @@ export default function ErrorMessage({ errorMessage }) {
   const previousURL = router.query.currentURL;
   const isHomePage = router.route === "/";
 
+  const { t: translate } = useTranslation("common");
+
   return (
     <StyledMessageWrapper>
-      <h2>Sorry, there was an error!</h2>
-      <b>You got redirected.</b>
-      <p>Error message:</p>
+      <h2>{translate("sorryError")}</h2>
+      <b>{translate("gotRedirected")}</b>
+      <p>{translate("typeOfErrorMessage")}</p>
       <p>{errorMessage}</p>
       <StyledErrorButtonBox>
         <StyledErrorButton
@@ -41,12 +44,12 @@ export default function ErrorMessage({ errorMessage }) {
             previousURL ? router.push(previousURL) : router.reload()
           }
         >
-          Try again
+          {translate("tryAgainButton")}
         </StyledErrorButton>
         <StyledErrorButton
           onClick={() => (isHomePage ? router.reload() : router.push("/"))}
         >
-          Homepage
+          {translate("backToHomepageButton")}
         </StyledErrorButton>
       </StyledErrorButtonBox>
     </StyledMessageWrapper>
