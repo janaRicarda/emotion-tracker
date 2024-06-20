727c38eeba82727ba404557837b3979ee4e7e4a7
Jana May <jana.ricarda.may@gmail.com>
2024-06-20 14:29:43 +0200

    fix login

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,42 +4,43 @@ import CredentialsProvider from "next-auth/providers/credentials";
 
 export const authOptions = {
   providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
-    }),
-    CredentialsProvider({
-      name: "credentials",
-      credentials: {
-        username: {
-          label: "Username",
-          type: "text",
-          placeholder: "username",
-        },
-        password: { label: "Password", type: "password" },
-      },
-      async authorize(credentials) {
-        if (
-          credentials.username === "feeling" &&
-          credentials.password === "feeling"
-        ) {
-          return {
-            name: "New feeling",
-            email: "test@example.com",
-          };
-        } else if (
-          credentials.username === "emotion" &&
-          credentials.password === "emotion"
-        ) {
-          return {
-            name: "emotion lover",
-            email: "emotion@example.com",
-          };
-        } else {
-          return null;
-        }
-      },
-    }),
+    process.env.VERCEL_ENV === "preview"
+      ? CredentialsProvider({
+          name: "credentials",
+          credentials: {
+            username: {
+              label: "Username",
+              type: "text",
+              placeholder: "username",
+            },
+            password: { label: "Password", type: "password" },
+          },
+          async authorize(credentials) {
+            if (
+              credentials.username === "feeling" &&
+              credentials.password === "feeling"
+            ) {
+              return {
+                name: "New feeling",
+                email: "test@example.com",
+              };
+            } else if (
+              credentials.username === "emotion" &&
+              credentials.password === "emotion"
+            ) {
+              return {
+                name: "emotion lover",
+                email: "emotion@example.com",
+              };
+            } else {
+              return null;
+            }
+          },
+        })
+      : GithubProvider({
+          clientId: process.env.GITHUB_ID,
+          clientSecret: process.env.GITHUB_SECRET,
+        }),
   ],
 };
 
