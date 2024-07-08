3ef91a2b9cf00a97c42f680cd679e65c4e81310f
Jan Schwarz <jan-paul@schw-a-rz.de>
2024-07-08 12:13:45 +0200

    styling animations added; landingPageStyling branch merged

---
+++ components/CircleAnimation.js
@@ -0,0 +1,134 @@
+import styled from "styled-components";
+import { keyframes } from "styled-components";
+import { breakpoints } from "@/utils/breakpoints";
+
+const circleAnimation = keyframes`
+0% { transform: translateY(-40vh); background: var(--joy); }
+14% {background: var(--surprise);}
+28% {background: var(--fear);}
+42% {background: var(--sadness);}
+56% {background: var(--contempt);}
+70% {background: var(--disgust);}
+84% {background: var(--anger);  transform: translateY(40vh);}
+100% {background: var(--anger); transform: translateY(10vh); }
+`;
+
+const joyAnimation = keyframes`
+0% {transform: translateY(0); opacity: 1;}
+100% {transform: translateY(-40vh); opacity: 0;}
+`;
+
+const sadnessAnimation = keyframes`
+0% {transform: translateX(0); opacity: 1;}
+100% {transform: translateX(25vh); opacity: 0; }
+`;
+
+const contemptAnimation = keyframes`
+0% {transform: translateX(0); opacity: 1;}
+100% {transform: translateX(-25vh); opacity: 0; }
+`;
+
+const surpriseAnimation = keyframes`
+0% {transform: translate(0); opacity: 1;}
+100% {transform: translate(20vh, -35vh); opacity: 0; }
+`;
+
+const angerAnimation = keyframes`
+0% {transform: translateY(0); opacity: 1;}
+100% {transform: translate(-20vh, -35vh); opacity: 0; }
+`;
+
+const fearAnimation = keyframes`
+0% {transform: translateY(0); opacity: 1;}
+100% {transform: translate(30vh, -20vh); opacity: 0; }
+`;
+
+const disgustAnimation = keyframes`
+0% {transform: translateY(0); opacity: 1;}
+100% {transform: translate(-30vh, -20vh); opacity: 0; }
+`;
+
+const StyledCircle = styled.div`
+  width: 8rem;
+  height: 8rem;
+  border-radius: 50%;
+  position: absolute;
+  animation: ${circleAnimation} 3s ease-in-out;
+  @media ${breakpoints.mobileLandscape} {
+    width: 6rem;
+    height: 6rem;
+  }
+  @media ${breakpoints.tablet} {
+    width: 8rem;
+    height: 8rem;
+  }
+`;
+
+const StyledSmallCircle = styled.div`
+  opacity: 0;
+  width: 2rem;
+  height: 2rem;
+  border-radius: 50%;
+  position: absolute;
+  top: 50%;
+  left: calc(50% - 1rem);
+  right: calc(50% - 1rem);
+  animation-duration: 1s;
+  animation-timing-function: linear;
+  animation-fill-mode: forwards;
+`;
+
+const StyledJoy = styled(StyledSmallCircle)`
+  background: var(--joy);
+  animation-name: ${joyAnimation};
+`;
+
+const StyledSurprise = styled(StyledSmallCircle)`
+  background: var(--surprise);
+  animation-name: ${surpriseAnimation};
+`;
+
+const StyledFear = styled(StyledSmallCircle)`
+  background: var(--fear);
+  animation-name: ${fearAnimation};
+`;
+
+const StyledSadness = styled(StyledSmallCircle)`
+  background: var(--sadness);
+  animation-name: ${sadnessAnimation};
+`;
+
+const StyledContempt = styled(StyledSmallCircle)`
+  background: var(--contempt);
+  animation-name: ${contemptAnimation};
+`;
+
+const StyledDisgust = styled(StyledSmallCircle)`
+  background: var(--disgust);
+  animation-name: ${disgustAnimation};
+`;
+
+const StyledAnger = styled(StyledSmallCircle)`
+  background: var(--anger);
+  animation-name: ${angerAnimation};
+`;
+
+export default function CircleAnimation({ showLogIn }) {
+  return (
+    <>
+      {showLogIn ? (
+        <>
+          <StyledJoy />
+          <StyledSurprise />
+          <StyledFear />
+          <StyledSadness />
+          <StyledContempt />
+          <StyledDisgust />
+          <StyledAnger />
+        </>
+      ) : (
+        <StyledCircle />
+      )}
+    </>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,9 @@ import { useRouter } from "next/router";
 import Icon from "@mdi/react";
 import { mdiGithub, mdiGoogle, mdiFlaskOutline, mdiAccount } from "@mdi/js";
 import { useSession } from "next-auth/react";
-import { keyframes } from "styled-components";
+import { keyframes, css } from "styled-components";
+import { breakpoints } from "@/utils/breakpoints";
+import CircleAnimation from "@/components/CircleAnimation";
 
 const fadeIn = keyframes`
 0% { opacity: 0;}
@@ -19,68 +21,135 @@ const buttonFadeIn = keyframes`
 100% {opacity: 1;}
 `;
 
+const buttonDisappears = keyframes`
+0% {opacity: 1; color: transparent; position: relative;}
+100% {opacity: 0; color: transparent; position: absolute;}
+`;
+
 const Wrapper = styled.div`
+  background: var(--section-background);
+  width: 100vw;
+  height: 100vh;
+  padding-top: 3rem;
   display: flex;
-  width: 75vw;
-  max-width: 500px;
   flex-direction: column;
-  justify-content: center;
+  justify-content: flex-start;
   align-items: center;
+
+  @media ${breakpoints.mobileLandscape} {
+    padding-top: 0;
+    padding-bottom: 1rem;
+    gap: 0;
+  }
+  @media ${breakpoints.tablet} {
+    padding-bottom: 0;
+    padding-top: 3rem;
+    gap: ${({ $showLogIn }) => ($showLogIn ? "2rem" : "0")};
+  }
 `;
 
 const StyledBigLogo = styled(BigLogo)`
-  max-width: 15rem;
-  max-height: 15rem;
+  max-width: 16rem;
+  max-height: 16rem;
+  opacity: 0;
   animation-name: ${fadeIn};
   animation-duration: 2s;
+  animation-delay: 1200ms;
   animation-timing-function: linear;
-  position: relative;
-  top: 0;
+  animation-fill-mode: forwards;
+  @media ${breakpoints.mobileLandscape} {
+    width: 8rem;
+    height: 8rem;
+  }
+  @media ${breakpoints.tablet} {
+    width: 16rem;
+    height: 16rem;
+  }
 `;
 
 const StartButton = styled(StyledButton)`
-  width: auto;
-  padding: 0.6rem;
+  width: 8rem;
+  height: 8rem;
+  background: conic-gradient(
+    #fbffc7,
+    #e2ffc7,
+    #c7ffe7,
+    #c7f3ff,
+    #d2c8ff,
+    #fbc8ff,
+    #ffc8c8,
+    #fbffc7
+  );
   border: none;
-  border-radius: 3px;
+  border-radius: 50%;
+  color: var(--text-on-bright);
+  font-weight: 500;
   opacity: 0;
-  animation: ${buttonFadeIn} 1s linear 1s;
+  animation-name: ${({ $showLogIn }) =>
+    $showLogIn
+      ? css`
+          ${buttonDisappears}
+        `
+      : css`
+          ${buttonFadeIn}
+        `};
+  animation-duration: ${({ $showLogIn }) => ($showLogIn ? "100ms" : "500ms")};
+  animation-timing-function: linear;
+  animation-delay: ${({ $showLogIn }) => ($showLogIn ? "none" : "3s")};
   animation-fill-mode: forwards;
+  @media ${breakpoints.mobileLandscape} {
+    width: 6rem;
+    height: 6rem;
+  }
+  @media ${breakpoints.tablet} {
+    width: 8rem;
+    height: 8rem;
+  }
 `;
 
 const StyledLogInWrapper = styled.div`
   display: grid;
+  justify-content: center;
   grid-template-rows: ${({ $show }) => ($show ? "1fr" : "0fr")};
   transition: grid-template-rows 1s;
-  width: 100%;
+  width: 80vw;
   margin: 2rem;
+`;
 
-  && > div {
-    overflow: hidden;
-    display: flex;
-    flex-direction: column;
-    gap: 1rem;
-  }
+const StyledLoginButtonWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 
-  && > div > h3 {
-    text-align: center;
-  }
+  /* overflow: hidden; */
+
+  /* margin-top: 1rem; */
+  padding: 1rem;
+  border-radius: 0.5rem;
+  background: conic-gradient(
+    #fbffc7,
+    #e2ffc7,
+    #c7ffe7,
+    #c7f3ff,
+    #d2c8ff,
+    #fbc8ff,
+    #ffc8c8,
+    #fbffc7
+  );
+  color: var(--text-on-bright);
 `;
 
 const LoginBox = styled.button`
   display: flex;
-  padding: 0.5rem;
-  border: none;
-  background-color: white;
-  color: black;
-  width: 100%;
+  padding: 0.8rem;
+  margin: 0.5rem auto;
+  background: var(--section-background);
+  color: var(--main-dark);
+  width: 80vw;
+  border-radius: 0.25rem;
   align-items: center;
   justify-content: center;
-  position: relative;
-
   & > svg {
-    position: absolute;
-    left: calc(50% - 5rem);
     margin-right: 0.6rem;
   }
 `;
@@ -116,6 +185,16 @@ const StyledCredentialForm = styled.form`
   && > * button {
     margin-top: 1rem;
   }
+`;
+
+const StyledParagraph = styled.p`
+  opacity: 0;
+  margin-top: ${({ $showLogIn }) => ($showLogIn ? "none" : "2rem")};
+  animation-name: ${fadeIn};
+  animation-duration: 1s;
+  animation-delay: 3500ms;
+  animation-timing-function: linear;
+  animation-fill-mode: forwards;
 `;
 
 export default function LandingPage({ handleDemoMode }) {
@@ -173,6 +252,7 @@ export default function LandingPage({ handleDemoMode }) {
         });
       }, 1);
     }
+
     if (state.show) {
       setFunction({ ...state, animation: !state.animation });
       setTimeout(() => {
@@ -182,106 +262,112 @@ export default function LandingPage({ handleDemoMode }) {
   }
 
   return (
-    <Wrapper>
-      <StyledBigLogo />
-      {!showLogIn.show ? (
-        <>
-          <StartButton
-            onClick={() => {
-              handleShowComponentAndAnimation(showLogIn, setShowLogIn);
-            }}
-          >
-            Get Started!
-          </StartButton>
-        </>
-      ) : (
-        <StyledLogInWrapper $show={showLogIn.animation}>
-          <div>
-            <h3>Login</h3>
-            {providers?.github && (
-              <LoginBox
-                onClick={() => signIn("github", { callbackUrl: "/home" })}
-              >
-                <Icon path={mdiGithub} size={1} />
-                GitHub
-              </LoginBox>
-            )}
-            {providers?.google && (
-              <LoginBox
-                onClick={() => signIn("google", { callbackUrl: "/home" })}
-              >
-                <Icon path={mdiGoogle} size={1} />
-                Google
-              </LoginBox>
-            )}
-            {providers?.credentials && (
-              <Accordeon>
-                <LoginBox
-                  onClick={() =>
-                    handleShowComponentAndAnimation(
-                      showCredentialsForm,
-                      setShowCredentialsForm
-                    )
-                  }
-                >
-                  <Icon path={mdiAccount} size={1} /> Credentials
-                </LoginBox>
-                {showCredentialsForm.show && (
-                  <StyledCredentialForm
-                    $show={showCredentialsForm.animation}
-                    onSubmit={handleCredentialLogin}
-                  >
-                    <div>
-                      <label htmlFor="username">Username</label>
-                      <input
-                        type="text"
-                        id="username"
-                        placeholder="username"
-                        onChange={(event) => {
-                          setUserCredentials({
-                            username: event.target.value,
-                          });
-                        }}
-                        required
-                      />
-                      <label htmlFor="password">Password</label>
-                      <input
-                        type="password"
-                        id="password"
-                        placeholder="password"
-                        onChange={(event) => {
-                          setUserCredentials({
-                            ...userCredentials,
-                            password: event.target.value,
-                          });
-                        }}
-                        required
-                      />
-                      {errorCredentialLogIn && (
-                        <>
-                          <b>
-                            Sorry, something&apos;s wrong. Please, try again.
-                          </b>
-                          <p>Error-Code: {errorCredentialLogIn.status}</p>
-                          <p>Message: {errorCredentialLogIn.error}</p>
-                        </>
-                      )}
-                      <button type="submit">Submit</button>
-                    </div>
-                  </StyledCredentialForm>
-                )}
-              </Accordeon>
-            )}
-            <LoginBox
+    <>
+      <CircleAnimation showLogIn={showLogIn.show} />
+      <Wrapper>
+        <StyledBigLogo />
+        {!showLogIn.show ? (
+          <>
+            <StartButton
               onClick={() => {
-                handleDemoMode();
+                handleShowComponentAndAnimation(showLogIn, setShowLogIn);
               }}
             >
-              <Icon path={mdiFlaskOutline} size={1} /> Demo-Mode
-            </LoginBox>
-          </div>
-        </StyledLogInWrapper>
-      )}
-    </Wrapper>
+              Get Started!
+            </StartButton>
+            <StyledParagraph $showLogIn={showLogIn.show}>
+              Your tool for tracking your emotions
+            </StyledParagraph>
+          </>
+        ) : (
+          <StyledLogInWrapper $show={showLogIn.animation}>
+            <StyledLoginButtonWrapper>
+              <h3>Login</h3>
+              {providers?.github && (
+                <LoginBox
+                  onClick={() => signIn("github", { callbackUrl: "/home" })}
+                >
+                  <Icon path={mdiGithub} size={1} />
+                  GitHub
+                </LoginBox>
+              )}
+              {providers?.google && (
+                <LoginBox
+                  onClick={() => signIn("google", { callbackUrl: "/home" })}
+                >
+                  <Icon path={mdiGoogle} size={1} />
+                  Google
+                </LoginBox>
+              )}
+              {providers?.credentials && (
+                <Accordeon>
+                  <LoginBox
+                    onClick={() =>
+                      handleShowComponentAndAnimation(
+                        showCredentialsForm,
+                        setShowCredentialsForm
+                      )
+                    }
+                  >
+                    <Icon path={mdiAccount} size={1} /> Credentials
+                  </LoginBox>
+                  {showCredentialsForm.show && (
+                    <StyledCredentialForm
+                      $show={showCredentialsForm.animation}
+                      onSubmit={handleCredentialLogin}
+                    >
+                      <div>
+                        <label htmlFor="username">Username</label>
+                        <input
+                          type="text"
+                          id="username"
+                          placeholder="username"
+                          onChange={(event) => {
+                            setUserCredentials({
+                              username: event.target.value,
+                            });
+                          }}
+                          required
+                        />
+                        <label htmlFor="password">Password</label>
+                        <input
+                          type="password"
+                          id="password"
+                          placeholder="password"
+                          onChange={(event) => {
+                            setUserCredentials({
+                              ...userCredentials,
+                              password: event.target.value,
+                            });
+                          }}
+                          required
+                        />
+                        {errorCredentialLogIn && (
+                          <>
+                            <b>
+                              Sorry, something&apos;s wrong. Please, try again.
+                            </b>
+                            <p>Error-Code: {errorCredentialLogIn.status}</p>
+                            <p>Message: {errorCredentialLogIn.error}</p>
+                          </>
+                        )}
+                        <button type="submit">Submit</button>
+                      </div>
+                    </StyledCredentialForm>
+                  )}
+                </Accordeon>
+              )}
+              <LoginBox
+                onClick={() => {
+                  handleDemoMode();
+                }}
+              >
+                <Icon path={mdiFlaskOutline} size={1} /> Demo-Mode
+              </LoginBox>
+            </StyledLoginButtonWrapper>
+          </StyledLogInWrapper>
+        )}
+      </Wrapper>
+    </>
   );
 }
